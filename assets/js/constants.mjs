bf997f3346185152250f0492ac14c0022543354c
Sigurd vaaler <sigvaa52915@stud.noroff.no>
2024-09-09 18:28:43 +0200

    api key hidden
    using vite to not show the api key in the repo

--- assets/js/constants.mjs
+++ assets/js/constants.mjs
@@ -1,5 +1,5 @@
-export const API_KEY = "233a5a18-686a-429b-864d-e65aa63b89da";
-export const API_BASE = "https://v2.api.noroff.dev";
+export const API_KEY = import.meta.env.VITE_API_KEY;
+export const API_BASE = import.meta.env.VITE_API_BASE;
 export const API_AUTH = "/auth";
 export const API_REGISTER = "/register";
 export const API_LOGIN = "/login";
@@ -7,3 +7,6 @@ export const API_KEY_URL = "/create-api-key";
 export const API_AUCTION = "/auction";
 export const API_LISTINGS_BASE = "/listings";
 export const ALL_LISTINGS_URL = `${API_BASE}${API_AUCTION}${API_LISTINGS_BASE}`;
+export const API_PROFILE = "/profiles";
+
+
