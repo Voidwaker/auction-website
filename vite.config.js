93f1c809866d88d6a40718f4a61a039de523b9c0
Sigurd vaaler <sigvaa52915@stud.noroff.no>
2024-09-14 22:46:29 +0200

    Create vite.config.js
    created vite.config,js

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        auctions: resolve(__dirname, 'pages/auctions.html'),
+        profile: resolve(__dirname, 'pages/profile.html'),
+        'listing-details': resolve(__dirname, 'pages/listing-details.html'),
+        registration: resolve(__dirname, 'registration.html'),
+      }
+    }
+  }
+});
+
