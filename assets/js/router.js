90f04c28d8f789b54e39e35fc43858269ea6cb72
Sigurd vaaler <sigvaa52915@stud.noroff.no>
2024-09-14 23:03:16 +0200

    updated navitgation

--- assets/js/router.js
+++ assets/js/router.js
@@ -3,13 +3,13 @@ window.addEventListener("load", handleRoute);
 
 function handleRoute() {
     const hashPath = window.location.hash || "#/";
-    const route = hashPath.slice(1); // Fjerner hash-tegnet (#)
+    const route = hashPath.slice(1); 
 
     if (route === "/auctions") {
         loadAuctionsPage();
     } else if (route === "/profile") {
         loadProfilePage();
     } else {
-        loadHomePage(); // Standardrute
+        loadHomePage(); 
     }
 }
