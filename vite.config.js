3a0cfdc8e38b53149f9b715e8e8ceb6a4b56fdb5
Sigurd vaaler <sigvaa52915@stud.noroff.no>
2024-09-14 23:32:52 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,14 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/auction-website/', 
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        auctions: resolve(__dirname, 'pages/auctions.html'),
-        profile: resolve(__dirname, 'pages/profile.html'),
-        'listing-details': resolve(__dirname, 'pages/listing-details.html'),
-        registration: resolve(__dirname, 'registration.html'),
+        main: resolve(__dirname, 'index.html')
       }
     }
   }
 });
+
 
 
