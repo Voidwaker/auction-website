9eece194da6b5899ae40d46777439f84263a7630
Sigurd vaaler <sigvaa52915@stud.noroff.no>
2024-09-16 14:05:05 +0200

    Update constants.mjs
    updating constants.mjs again trying to fix registration and login errors

--- assets/js/constants.mjs
+++ assets/js/constants.mjs
@@ -1,17 +1,10 @@
-/* export const API_KEY = import.meta.env.VITE_API_KEY;
-export const API_BASE = import.meta.env.VITE_API_BASE;
-
-console.log(import.meta.env);
- */
-
+export const API_KEY = "233a5a18-686a-429b-864d-e65aa63b89da";
+export const API_BASE = "https://v2.api.noroff.dev";  // Forsikre deg om at dette er riktig
 export const API_AUTH = "/auth";
-export const API_REGISTER = "/register";
+export const API_REGISTER = "/auth/register";  // Endret til riktig registreringsrute
 export const API_LOGIN = "/auth/login";
-export const API_KEY_URL = "/create-api-key";
 export const API_AUCTION = "/auction";
 export const API_LISTINGS_BASE = "/listings";
 export const ALL_LISTINGS_URL = `${API_BASE}${API_AUCTION}${API_LISTINGS_BASE}`;
-
-
 
 
