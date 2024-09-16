4b27ee284bff4f9b758545d531f494137e772b6a
Sigurd vaaler <sigvaa52915@stud.noroff.no>
2024-09-16 15:14:44 +0200

    404 errors now again

--- assets/js/constants.mjs
+++ assets/js/constants.mjs
@@ -1,10 +1,9 @@
 export const API_KEY = "233a5a18-686a-429b-864d-e65aa63b89da";
-export const API_BASE = "https://v2.api.noroff.dev";  // Forsikre deg om at dette er riktig
+export const API_BASE = "https://v2.api.noroff.dev";
 export const API_AUTH = "/auth";
-export const API_REGISTER = "/auth/register";  // Endret til riktig registreringsrute
-export const API_LOGIN = "/login";
+export const API_REGISTER = "/auth/register";
+export const API_LOGIN = "/auth/login"; 
 export const API_AUCTION = "/auction";
 export const API_LISTINGS_BASE = "/listings";
 export const ALL_LISTINGS_URL = `${API_BASE}${API_AUCTION}${API_LISTINGS_BASE}`;
-
 
