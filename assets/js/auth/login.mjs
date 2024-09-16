3fad54139f9c6ee468f16572ce154fb214b15900
Sigurd vaaler <sigvaa52915@stud.noroff.no>
2024-09-16 13:41:53 +0200

    Update login.mjs
    updated error handeling (doe to errors while trying to log in on mac

--- assets/js/auth/login.mjs
+++ assets/js/auth/login.mjs
@@ -3,45 +3,34 @@ import { API_BASE, API_LOGIN } from "../constants.mjs";
 
 export async function login(email, password) {
     try {
-
         const response = await fetch(`${API_BASE}${API_LOGIN}`, {
             headers: { "Content-Type": "application/json" },
             method: "POST",
             body: JSON.stringify({ email, password }),
         });
 
-        console.log("Login response:", response); 
-
-        if (response.ok) {
-
-            const { accessToken, ...profile } = (await response.json()).data;
-
-            save("Token", accessToken);
-            save("Profile", profile);
-
-            const modal = bootstrap.Modal.getInstance(document.getElementById('loginModal'));
-            if (modal) {
-                modal.hide();
-            }
-
-            console.log("Redirecting to profile...");
-
-            window.location.hash = "#/profile"; 
-
-            console.log("Hash set to: ", window.location.hash);
-
-            return profile;
-        } else {
-
+        if (!response.ok) {
             const errorData = await response.json();
-            console.error("Error response from server:", errorData);
+            throw new Error(`Login failed: ${errorData.errors[0].message || response.statusText}`);
         }
-    } catch (error) {
-        console.error("Failed to login:", error); 
-    }
 
-    throw new Error("Failed to login");
+        const { accessToken, ...profile } = (await response.json()).data;
+        save("Token", accessToken);
+        save("Profile", profile);
+
+        const modal = bootstrap.Modal.getInstance(document.getElementById('loginModal'));
+        if (modal) {
+            modal.hide();
+        }
+
+        window.location.hash = "#/profile"; 
+        return profile;
+    } catch (error) {
+        console.error("Login error:", error);
+        alert(error.message); 
+    }
 }
+
 
 
 
